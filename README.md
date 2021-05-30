aa15b04eacdbaf00c843d633cc1ab1bd7c8a3ed3
Fullchee Zhang <fullchee@gmail.com>
2021-05-29 21:56:48 -0400

    Add / ping endpoint

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Previous API
+* https://github.com/Fullchee/reminders-backend/blob/master/src/index.js
+
+Previous TODO
+* https://github.com/Fullchee/reminders-backend
+
+Started with this Tutorial
+* https://www.youtube.com/playlist?list=PL-51WBLyFTg2vW-_6XBoUpE7vpmoR3ztO
---
+++ api/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+
+def ping(request):
+    return HttpResponse("Pong!")
+
+
+def random_link(request):
+    return JsonResponse({})
+
+
+def all_links(request):
+    return JsonResponse({})
+
+
+def keywords(request):
+    return JsonResponse({})
+
+
+def link(request):
+    return JsonResponse({})
+
+
+def search(request):
+    return JsonResponse({})
+
+
+def add_link(request):
+    return JsonResponse({})
+
+
+def delete_link(request):
+    return JsonResponse({})
+
+
+def update_link(request):
+    return JsonResponse({})
