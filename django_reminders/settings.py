d7974902e75c5032fc4b48eaff098c286cff5a9e
Fullchee Zhang <fullchee@gmail.com>
2021-06-09 23:25:43 -0400

    Remove unused decouple package

--- django_reminders/settings.py
+++ django_reminders/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-from decouple import config
 import environ
 
 root = environ.Path(__file__) - 3  # get root of the project
