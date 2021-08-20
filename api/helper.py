117b1f876899c261eb7823edb9cba78e2ab8c7da
Fullchee Zhang <fullchee@gmail.com>
2021-08-20 17:13:43 -0400

    shorten youtube url: don't lose the time stamp

--- api/helper.py
+++ api/helper.py
@@ -10,14 +10,14 @@ def parse_keywords(keywords):
     return list(map(lambda keyword: keyword["value"], keywords))
 
 
-def get_youtube_id(url: str):
+def get_youtube_id(url: str) -> str:
     desktop_match = search(
         "youtube\.com/watch\?v=([a-zA-Z0-9-_]{11})",
         url,
     )
 
     shortened_youtube_match = search(
-        'https://youtu.be/([a-zA-Z0-9-_]{11})',
+        "https://youtu.be/([a-zA-Z0-9-_]{11})",
         url,
     )
     google_mobile_match = search(
@@ -34,18 +34,28 @@ def get_youtube_id(url: str):
             try:
                 return shortened_youtube_match.groups()[0]
             except Exception:
-                return None
+                return ""
 
 
+def get_youtube_time(url: str) -> str:
+    time_match = search(
+        "(\?t=\d+)$",
+        url,
+    )
+    try:
+        return time_match.groups()[0]
+    except Exception:
+        return ""
 
-def shorten_youtube_url(url: str):
+
+def shorten_youtube_url(url: str) -> str:
     youtube_id = get_youtube_id(url)
     if youtube_id:
-        return f"https://youtu.be/{youtube_id}"
+        return f"https://youtu.be/{youtube_id}{get_youtube_time(url)}"
     return url
 
 
-def generate_youtube_title(url: str):
+def generate_youtube_title(url: str) -> str:
     """
     https://stackoverflow.com/a/52664178/8479344
     :param url: str
@@ -53,7 +63,7 @@ def generate_youtube_title(url: str):
     """
     youtube_id = get_youtube_id(url)
     if not youtube_id:
-        return ''
+        return ""
     params = {
         "format": "json",
         "url": "https://www.youtube.com/watch?v=%s" % youtube_id,
