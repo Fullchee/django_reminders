b0f8f3d4ceb7ad89d4dfc8233659fdebc24912b6
Fullchee Zhang <fullchee@gmail.com>
2024-02-20 02:30:06 -0500

    Move old code to views_sql

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 from . import views
+from . import views_sql
 
 urlpatterns = [
     path("", views.redirect_to_frontend),
@@ -9,13 +10,13 @@ urlpatterns = [
     path("v1/links", views.LinkView.as_view(), name="links"),
     path("tinymce/", include("tinymce.urls")),
     # TODO: deprecate these v0 link endpoints
-    path("links", views.get_all_links),
-    path("links/<int:link_id>", views.get_link),
-    path("link/<int:link_id>", views.get_link),
-    path("random-link", views.get_random_link_sql),
-    path("search", views.search),
-    path("add-link", views.add_link),
-    path("update-link", views.update_link_sql),
-    path("delete-link", views.delete_link),
+    path("links", views_sql.get_all_links),
+    path("links/<int:link_id>", views_sql.get_link),
+    path("link/<int:link_id>", views_sql.get_link),
+    path("random-link", views_sql.get_random_link_sql),
+    path("search", views_sql.search),
+    path("add-link", views_sql.add_link),
+    path("update-link", views_sql.update_link_sql),
+    path("delete-link", views_sql.delete_link),
     # end of deprecated link endpoints
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,8 @@
-import json
 import logging
-from operator import itemgetter
 from typing import Optional
-from urllib.error import HTTPError
 
 from django.db import connection
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -16,19 +11,17 @@ from rest_framework.views import APIView
 from api.models import Link
 from api.responses import (
     JsonResponse,
-    JsonResponseForbidden,
     JsonResponseMethodNotAllowed,
     JsonResponseNotFound,
-    JsonResponseServerError,
 )
-from api.services.link_services import create_link, get_random_link, search_links
+from api.services.link_services import (
+    create_link,
+    get_random_link,
+    search_links,
+    update_link,
+)
 from api.services.raw_sql import fetchall_as_dict, sql_text
-from api.services.youtube import (
-    calculate_title,
-    calculate_youtube_url,
-    extract_youtube_info,
-    generate_youtube_title,
-)
+from api.services.youtube import calculate_title, calculate_youtube_url
 
 logger = logging.getLogger(__name__)
 
@@ -51,11 +44,6 @@ def get_keywords(request):
         )
         result = fetchall_as_dict(cursor)
         return JsonResponse(sorted(map(lambda obj: obj["keyword"], result)))
-
-
-def parse_keywords(keywords):
-    """TODO: move into a serializer"""
-    return list(map(lambda keyword: keyword["value"], keywords))
 
 
 class LinkView(APIView):
@@ -126,7 +114,7 @@ class LinkView(APIView):
         if link_id:
             serializer = self.UpdateInputSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-            update_link_sql(**serializer.validated_data)
+            update_link(**serializer.validated_data)
         else:
             serializer = self.InputSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
@@ -136,195 +124,3 @@ class LinkView(APIView):
         if not link_id:
             return JsonResponseMethodNotAllowed()
         return JsonResponse({"success": "Deleted link"})
-
-
-# @api_view(['GET'])
-def get_all_links(request):
-    # links = list(Link.objects.filter(user=request.user))
-    # return HttpResponse(serialize('json', links), content_type='application/json')
-    with connection.cursor() as cursor:
-        cursor.execute(sql_text("""SELECT * FROM api_link"""))
-        result = fetchall_as_dict(cursor)
-        return JsonResponse(result)
-
-
-# @api_view(['GET'])
-def get_random_link_sql(request):
-    with connection.cursor() as cursor:
-        cursor.execute(
-            sql_text(
-                """
-            SELECT * FROM api_link
-            ORDER BY RANDOM()
-            LIMIT 1
-        """
-            )
-        )
-        result = fetchall_as_dict(cursor)
-        return JsonResponse(result[0])
-
-
-# @api_view(['GET'])
-def get_link(request, link_id):
-    with connection.cursor() as cursor:
-        cursor.execute(
-            sql_text(
-                """
-            SELECT *
-            FROM api_link
-            WHERE id = :link_id
-        """
-            ),
-            {"link_id": link_id},
-        )
-        result = fetchall_as_dict(cursor)[0]
-        if "youtu.be" in result["url"]:
-            result["url"] += f"?t={result['start_time'] or 0}"
-        result["startTime"] = result["start_time"]
-        return JsonResponse(result)
-
-
-# @api_view(['GET'])
-def search(request):
-    with connection.cursor() as cursor:
-        cursor.execute(
-            sql_text(
-                """
-            SELECT *
-            FROM api_link
-            WHERE (
-              title ILIKE '%' || :query || '%'
-              OR
-              notes ILIKE '%' || :query || '%'
-              OR
-              :query = ANY(keywords)
-              )
-        """
-            ),
-            {"query": request.GET.get("q", "")},
-        )
-        result = fetchall_as_dict(cursor)
-        return JsonResponse(result)
-
-
-def add_or_update(body, action: str, query: str) -> HttpResponse:
-    """
-    :param body:
-    :param action: 'add' or 'update'
-    :param query: insert or update query
-    """
-    notes, title, url, keywords = itemgetter(
-        "notes",
-        "title",
-        "url",
-        "keywords",
-    )(body)
-
-    link_id = flag = start_time = None
-    if action == "update":
-        link_id, flag, start_time = itemgetter("id", "flag", "start_time")(body)
-
-    keywords = parse_keywords(keywords)
-    url, youtube_start_time = extract_youtube_info(url)
-    try:
-        title = title or generate_youtube_title(url)
-    except HTTPError as e:
-        logger.error(repr(e))
-        if e.code == 401:
-            error_message = "The YouTube video's author may have disabled playback outside of YouTube"
-            logger.error(error_message)
-            return JsonResponseForbidden({"message": error_message})
-        # return JsonResponseServerError(e)
-    except Exception as e:
-        logger.error(e)
-        return JsonResponseServerError()
-
-    # prioritize the form start time over the YouTube URL (which might be outdated)
-    start_time = (start_time and int(start_time)) or youtube_start_time or 0
-
-    with connection.cursor() as cursor:
-        cursor.execute(
-            sql_text(query),
-            {
-                "id": link_id,
-                "notes": notes,
-                "title": title,
-                "url": url,
-                "keywords": keywords,
-                "flag": flag,
-                "start_time": start_time,
-            },
-        )
-        result = fetchall_as_dict(cursor)[0]
-        if "youtu.be" in result["url"]:
-            result["url"] += f"?t={result['start_time'] or 0}"
-        result["startTime"] = result["start_time"]
-        return JsonResponse(result)
-
-
-@csrf_exempt
-# @api_view(['POST', 'PUT'])
-def add_link(request):
-    # TODO: auth with request.user.id
-
-    # temp measure, the @api_view decorator will do this in the future
-    if request.method != "POST" and request.method != "PUT":
-        return HttpResponse(status=405)
-
-    INSERT_QUERY = """
-        INSERT INTO api_link (id, notes, title, url, keywords, last_accessed, user_id, flag, start_time)
-        VALUES (nextval('api_link_id_seq'::regclass), :notes, :title, :url, :keywords, NOW(), 2, FALSE, :start_time)
-        ON CONFLICT (url) DO UPDATE SET url = :url
-        RETURNING *
-    """
-    return add_or_update(json.loads(request.body), "add", query=INSERT_QUERY)
-
-
-@csrf_exempt
-# @api_view(['POST', 'PUT'])
-def update_link_sql(request) -> HttpResponse:
-    # TODO: auth with request.user.id
-
-    # temp measure, the @api_view decorator will do this in the future
-    if request.method != "POST" and request.method != "PUT":
-        return HttpResponse(status=405)
-    UPDATE_QUERY = """
-        UPDATE api_link
-        SET keywords = :keywords,
-          title = :title,
-          url = :url,
-          notes = :notes,
-          flag = :flag,
-          last_accessed = NOW(),
-          start_time = :start_time,
-          views =
-            CASE last_accessed = CURRENT_DATE
-              WHEN TRUE THEN views
-              WHEN FALSE THEN views + 1
-            END
-        WHERE id = :id
-        RETURNING *
-    """
-    return add_or_update(json.loads(request.body), "update", query=UPDATE_QUERY)
-
-
-@csrf_exempt
-# @api_view(['POST', 'PUT'])
-def delete_link(request) -> HttpResponse:
-    if request.method != "POST" and request.method != "PUT":
-        return HttpResponse(status=405)
-    body = json.loads(request.body)
-    link_id = body["id"]
-    with connection.cursor() as cursor:
-        cursor.execute(
-            sql_text(
-                """
-            DELETE FROM api_link
-            WHERE id = :id
-            RETURNING :id as id
-        """
-            ),
-            {"id": link_id},
-        )
-        result = fetchall_as_dict(cursor)
-        return JsonResponse(result[0])
